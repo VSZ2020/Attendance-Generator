c49ac28014047644a4931d523ca01a6111836220
IVS <v.iz.19@ya.ru>
2025-04-23 09:20:03 +0500

    fix-2025-04-23

--- AG.Data/Entities/RelationshipTables/EmployeeToTimeInterval.cs
+++ AG.Data/Entities/RelationshipTables/EmployeeToTimeInterval.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using AG.Core.Enums;
 
 namespace AG.Data.Entities.RelationshipTables
 {
     public class EmployeeToTimeInterval : BaseEntity
     {
         public Guid EmployeeId { get; set; }
-
-        public Guid TimeIntervalId { get; set; }
+        public EmployeeEntity? Employee { get; set; }
+        
+        public DayType TimeIntervalType { get; set; }
 
         [Required]
         public DateTime Begin { get; set; }
